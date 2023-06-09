3b3ab345cd34104102c77f3bc0998cc15ce8ba0d
Max <maxhirning25@gmail.com>
2023-06-09 12:09:39 +0300

    Added device info

--- src/views/Settings/Settings.tsx
+++ src/views/Settings/Settings.tsx
@@ -1,6 +1,8 @@
-import { View } from "react-native";
+import { Text, View } from "react-native";
 import React, { ReactElement } from "react";
+import DeviceInfo from "react-native-device-info";
 import { themes } from "../../models/theme/theme";
+import { styles } from "../../models/theme/styles";
 import ChevronDownIcon from "../../icons/chevronDown";
 import { getAppTheme } from "../../controllers/themes";
 import { useDispatch, useSelector } from "react-redux";
@@ -20,35 +22,41 @@ export default function Settings(): ReactElement {
 	const buttonText = (): string => `${appTheme.charAt(0).toUpperCase() + appTheme.slice(1)} theme`;
 
 	return (
-		<View className="flex-1 items-center" style={{ backgroundColor: (getAppTheme(appTheme)).background }} >
-			<SelectDropdown
-				data={themes}
-				buttonStyle={{
-					width: 200,
-					borderWidth: 1,
-					borderRadius: 10,
-					borderColor: getAppTheme(appTheme).text,
-					backgroundColor: getAppTheme(appTheme).background,
-				}}
-				rowTextStyle={{
-					color: getAppTheme(appTheme).text,
-				}}
-				dropdownStyle={{
-					borderWidth: 1,
-					borderBottomLeftRadius: 10,
-					borderBottomRightRadius: 10,
-					borderColor: getAppTheme(appTheme).text,
-					backgroundColor: getAppTheme(appTheme).background,
-				}}
-				buttonTextStyle={{
-					color: getAppTheme(appTheme).text,
-				}}
-				defaultValue={appTheme}
-				onSelect={appThemeChange}
-				defaultButtonText={buttonText()}
-				buttonTextAfterSelection={buttonText}
-				renderDropdownIcon={(): ReactElement => <ChevronDownIcon color={getAppTheme(appTheme).icon} width={35} height={35}/>}
-			/>
+		<View className="flex-1 items-center justify-between pt-2 pb-5" style={{ backgroundColor: (getAppTheme(appTheme)).background }} >
+			<View>
+				<SelectDropdown
+					data={themes}
+					buttonStyle={{
+						width: 200,
+						borderWidth: 1,
+						borderRadius: 10,
+						borderColor: getAppTheme(appTheme).text,
+						backgroundColor: getAppTheme(appTheme).background,
+					}}
+					rowTextStyle={{
+						color: getAppTheme(appTheme).text,
+					}}
+					dropdownStyle={{
+						borderWidth: 1,
+						borderBottomLeftRadius: 10,
+						borderBottomRightRadius: 10,
+						borderColor: getAppTheme(appTheme).text,
+						backgroundColor: getAppTheme(appTheme).background,
+					}}
+					buttonTextStyle={{
+						color: getAppTheme(appTheme).text,
+					}}
+					defaultValue={appTheme}
+					onSelect={appThemeChange}
+					defaultButtonText={buttonText()}
+					buttonTextAfterSelection={buttonText}
+					renderDropdownIcon={(): ReactElement => <ChevronDownIcon color={getAppTheme(appTheme).icon} width={35} height={35}/>}
+				/>
+			</View>
+			<Text
+				className="text-sm"
+				style={[styles.fontFamilyText, {color: getAppTheme(appTheme).text}]}
+			>App version: {DeviceInfo.getVersion()}</Text>
 		</View>
 	);
 }
